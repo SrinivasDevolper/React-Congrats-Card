4677cd1c42806c9d8318eaf3884eabbc933a8e08
srinivasDevolper <kingsrinivassri123@gmail.com>
2024-04-26 06:42:32 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div class='bg-container'>
+    <h1>Congratulations</h1>
+    <div class='bg-content'>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' />
+      <h1>Kiran V</h1>
+      <p>Vishnu Instution Of Computer Educations and Technology, Bhimavaram</p>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
